d726394024323b5a9f5bf9e1ac8633eca60b7e6f
eric07109 <eric07109@gmail.com>
2018-05-13 16:01:43 +0800

    added lastname and firstname columns to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180513073223) do
+ActiveRecord::Schema.define(version: 20180513074027) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20180513073223) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.string "lastname"
+    t.string "firstname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
