c85e111e74418a3c53c5ecfcd5504d3252a62064
eric07109 <eric07109@gmail.com>
2018-05-30 07:52:20 +0800

    added description column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180520041433) do
+ActiveRecord::Schema.define(version: 20180529235106) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 20180520041433) do
     t.string "avatar"
     t.string "lastname"
     t.string "firstname"
+    t.string "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
