4291ce18aaa0723d4d2c269f8de19842ea2832c8
eric07109 <eric07109@gmail.com>
2018-06-09 17:36:07 +0800

    create friend list in user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,8 @@ class UsersController < ApplicationController
 		@user_comments = @user.comments
 		@user_unpublished_posts = @user.unpublished_posts
 		@user_collected_posts = @user.collected_posts
+		@friends_to_be_approved = @user.friends_to_be_approved
+		@friends_to_approve = @user.friends_to_approve
+		@friends = @user.accepted_friends + @user.approved_friends
 	end
 end
