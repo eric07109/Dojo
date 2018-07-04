a17334214db8b0202a7039f7da5f7547f00a76a8
eric07109 <eric07109@gmail.com>
2018-07-04 20:29:00 +0800

    added column admin to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_08_095457) do
+ActiveRecord::Schema.define(version: 2018_07_04_122623) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 2018_06_08_095457) do
     t.string "lastname"
     t.string "firstname"
     t.string "description"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
