94e152e21c76e085af5e4dc44d4dbb36bd976fe1
eric07109 <eric07109@gmail.com>
2018-07-04 20:23:56 +0800

    implement readable post logic on Comment.create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@ class CommentsController < ApplicationController
 	before_action :authenticate_user!
 	
 	def create
-		@comment = current_user.comments.build(comment_params)
+		@post = Post.find(params[:post_id])
+		@comment = current_user.comments.build(comment_params) if Post.readable_by(current_user).include?(@post)
 		if @comment.save
 			redirect_back fallback_location: posts_path
 		end
