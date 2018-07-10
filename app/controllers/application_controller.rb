14edb7e7d3ed4a85a2d6e73c09e1b8f8a407d4bc
eric07109 <eric07109@gmail.com>
2018-07-10 21:38:04 +0800

    implemented admin check for category methods and flash alert message if violated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def authenticate_admin
+    redirect_to root_path, alert: "Permission Deniedd" unless current_user.admin?
+  end
+
   protected
   def configure_permitted_parameters
   	devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :avatar])
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,28 +1,41 @@
 class CategoriesController < ApplicationController
 	before_action :authenticate_user!
+	before_action :authenticate_admin, only: [:index, :destroy, :update, :create]
 	def index
 		@categories = Category.all
 	end
 
 	def destroy
 		@category = Category.find(params[:id])
-		@category.destroy
-		redirect_back fallback_location: @root
+		if @category.post_category_mappings.present?
+			flash[:alert] = "Category #{@category.name} is still being used. The delete action is denied"
+			redirect_to categories_path
+		else 
+			@category.destroy
+			redirect_back fallback_location: categories_path
+		end
 	end
 
 	def create
 		@category = Category.new(category_params)
 		# Todo: catcth unique constraint
-		if @category.save
-			redirect_back fallback_location: @root
+		begin 
+			@category.save
+		rescue ActiveRecord::RecordNotUnique
+			flash[:alert] = "Category #{@category.name} has already existed. Two identical categories are not allowed."
 		end
+		redirect_back fallback_location: categories_path
 
 	end
 
 	def update
 		@category = Category.find(params[:id])
-		@category.update(category_params)
-		redirect_back fallback_location: @root
+		begin 
+			@category.update(category_params)
+		rescue ActiveRecord::RecordNotUnique
+			flash[:alert] = "Category #{@category.name} has already existed. Two identical categories are not allowed."
+		end
+		redirect_back fallback_location: categories_path
 	end
 
 	private
