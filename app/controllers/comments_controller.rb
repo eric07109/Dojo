d120589af69dfef926099bd6eb23121617433a49
eric07109 <eric07109@gmail.com>
2018-07-15 21:04:09 +0800

    implemented delete comment for comment author and admin

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,12 @@ class CommentsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@comment = Comment.find(params[:id])
+		@comment.destroy
+		redirect_back fallback_location: posts_path
+	end
+
 	private
 	def comment_params
 		params.permit(:content, :post_id)
