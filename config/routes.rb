3f60d2eabeb346494df1ec6cc04d50e82e3b2de9
eric07109 <eric07109@gmail.com>
2018-07-15 21:48:25 +0800

    set api routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,4 +30,10 @@ Rails.application.routes.draw do
     # resources :users, only: [:index, :update]
   end
 
+  namespace :api, defaults: {format: :json} do
+    namespace :vi do
+      resources :posts, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+
 end
